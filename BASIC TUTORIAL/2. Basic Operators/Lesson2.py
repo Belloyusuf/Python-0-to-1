334915fcdadab878275239f55365468571145bf2
bello yusuf <belloyusuf1997@gmail.com>
2023-11-08 03:08:47 +0100

    Done with assignment operators.

--- BASIC TUTORIAL/2. Basic Operators/Lesson2.py
+++ BASIC TUTORIAL/2. Basic Operators/Lesson2.py
@@ -1,3 +1,43 @@
 #1 usr/bin/python3
 
 # Python Comparison Operators
+
+"""
+==  Equall Operator
+!=  Not Equall Operator
+>   Greater than Operator
+<   Less than Operator 
+>=  Greater than or equall 
+<=  Less than or equall
+
+"""
+
+a = 21
+b = 10
+
+if ( a == b ):
+    print ("Line 1 - a is equal to b")
+else:
+    print ("Line 1 - a is not equal to b")
+if ( a != b ):
+    print ("Line 2 - a is not equal to b")
+else:
+    print ("Line 2 - a is equal to b")
+if ( a < b ):
+    print ("Line 3 - a is less than b" )
+else:
+    print ("Line 3 - a is not less than b")
+if ( a > b ):
+    print ("Line 4 - a is greater than b")
+else:
+    print ("Line 4 - a is not greater than b")
+
+a,b=b,a #values of a and b swapped. a becomes 10, b becomes 21
+if ( a <= b ):
+    print ("Line 5 - a is either less than or equal to b")
+else:
+    print ("Line 5 - a is neither less than nor equal to b")
+if ( b >= a ):
+    print ("Line 6 - b is either greater than or equal to b")
+else:
+    print ("Line 6 - b is neither greater than nor equal to b")
