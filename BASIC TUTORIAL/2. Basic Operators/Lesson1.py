8b8108c8e26d1f975d52602caad3c9c659fa037d
bello yusuf <belloyusuf1997@gmail.com>
2023-11-08 01:21:18 +0100

    First commit on python tutorials

---
+++ BASIC TUTORIAL/2. Basic Operators/Lesson1.py
@@ -0,0 +1,14 @@
+#! usr/bin/python3
+
+"""
+Python language supports the following types of operators-
+
+ 1. Arithmetic Operators
+ 2. Comparison (Relational) Operators
+ 3. Assignment Operators
+ 4. Logical Operators
+ 5. Bitwise Operators
+ 6. Membership Operators
+ 7. Identity Operators
+
+"""
---
+++ BASIC TUTORIAL/5. Numbers/Lesson1.py
@@ -0,0 +1,10 @@
+"""
+Python has five standard data types-
+
+1. Numbers
+2. String
+3. List
+4. Tuple
+5. Dictionary
+
+"""
