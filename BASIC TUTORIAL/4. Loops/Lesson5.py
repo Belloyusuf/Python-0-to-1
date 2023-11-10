03c6849709cd5fae379e572ff5892b767a0b18d3
bello yusuf <belloyusuf1997@gmail.com>
2023-11-10 02:40:52 +0100

    Done with lesson 5 moving to lesson 6

---
+++ BASIC TUTORIAL/4. Loops/Lesson5.py
@@ -0,0 +1,12 @@
+#! usr/bin/python3
+
+Friends = ["Alhaji", "Sule", "Yusuf", "Mansur"]
+
+index = 0
+
+for  f in Friends:
+    print(index, f)
+    index += 1
+
+# This would display the friend list with the index number. 
+# And the index start from 0 up to n
