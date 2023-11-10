919167aed8c6c132275e494ea62be8859ce5ca93
bello yusuf <belloyusuf1997@gmail.com>
2023-11-10 02:33:13 +0100

    Done with lesson 4 under loops.

---
+++ BASIC TUTORIAL/4. Loops/Lesson4.py
@@ -0,0 +1,18 @@
+#! usr/bin/python3
+
+# for Loop Statements
+
+"""
+The for statement in Python has the ability to iterate over the items of any sequence, such
+as a list or a string.
+
+
+Syntax
+-------
+for iterating_var in sequence:
+    statements(s)
+"""
+
+for i in range(10):
+    print(i)
+
