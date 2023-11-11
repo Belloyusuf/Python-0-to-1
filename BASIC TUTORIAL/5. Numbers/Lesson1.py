c1fa2fe3ee08da64f31ad489e5e814b5bf6551cf
bello yusuf <belloyusuf1997@gmail.com>
2023-11-11 11:51:56 +0100

    Working on numbers

--- BASIC TUTORIAL/5. Numbers/Lesson1.py
+++ BASIC TUTORIAL/5. Numbers/Lesson1.py
@@ -8,3 +8,47 @@ Python has five standard data types-
 5. Dictionary
 
 """
+
+num1 = 100
+num2 = 150
+
+print(num1 + num2)
+
+print ("abs(-45) : ", abs(-45))
+print ("abs(100.12) : ", abs(100.12))
+
+
+import math  # This will import math module
+# The following example shows the usage of the ceil() method.
+
+print ("math.ceil(-45.17) : ", math.ceil(-45.17))
+print ("math.ceil(100.12) : ", math.ceil(100.12))
+print ("math.ceil(100.72) : ", math.ceil(100.72))
+print ("math.ceil(math.pi) : ", math.ceil(math.pi))
+
+# The following example shows the usage of exp() method.
+print ("math.exp(-45.17) : ", math.exp(-45.17))
+print ("math.exp(100.12) : ", math.exp(100.12))
+print ("math.exp(100.72) : ", math.exp(100.72))
+print ("math.exp(math.pi) : ", math.exp(math.pi))
+
+
+# The following example shows the usage of the max() method.
+print ("max(80, 100, 1000) : ", max(80, 100, 1000))
+print ("max(-20, 100, 400) : ", max(-20, 100, 400))
+print ("max(-80, -20, -10) : ", max(-80, -20, -10))
+print ("max(0, 100, -400) : ", max(0, 100, -400))
+
+
+# The following example shows the usage of the min() method.
+print ("min(80, 100, 1000) : ", min(80, 100, 1000))
+print ("min(-20, 100, 400) : ", min(-20, 100, 400))
+print ("min(-80, -20, -10) : ", min(-80, -20, -10))
+print ("min(0, 100, -400) : ", min(0, 100, -400))
+
+
+# The following example shows the usage of the pow() method.
+print ("math.pow(100, 2) : ", math.pow(100, 2))
+print ("math.pow(100, -2) : ", math.pow(100, -2))
+print ("math.pow(2, 4) : ", math.pow(2, 4))
+print ("math.pow(3, 0) : ", math.pow(3, 0))
