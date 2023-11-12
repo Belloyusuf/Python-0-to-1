80ce5380da43065e0b64f0422535174316cda78a
bello yusuf <belloyusuf1997@gmail.com>
2023-11-12 12:15:20 +0100

    Done with dict

--- BASIC TUTORIAL/9. Dictionary/Lesson2.py
+++ BASIC TUTORIAL/9. Dictionary/Lesson2.py
@@ -8,3 +8,12 @@ sqrt = {x: x**2 for x in (2, 4, 6)}
 print(sqrt)
 
 dict(sape=4139, guido=4127, jack=4098)
+
+
+# Update Dictionary
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; # update existing entry
+
+dict['School'] = "DPS School" # Add new entry
+print ("dict['Age']: ", dict['Age'])
+print ("dict['School']: ", dict['School'])
