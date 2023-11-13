73b187d2df034859794102f7e8797eb72134cb48
bello yusuf <belloyusuf1997@gmail.com>
2023-11-13 07:46:31 +0100

    Done with lesson 2 on dictionary

--- BASIC TUTORIAL/9. Dictionary/Lesson2.py
+++ BASIC TUTORIAL/9. Dictionary/Lesson2.py
@@ -14,6 +14,7 @@ dict(sape=4139, guido=4127, jack=4098)
 dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
 dict['Age'] = 8; # update existing entry
 
+
 dict['School'] = "DPS School" # Add new entry
 print ("dict['Age']: ", dict['Age'])
 print ("dict['School']: ", dict['School'])
