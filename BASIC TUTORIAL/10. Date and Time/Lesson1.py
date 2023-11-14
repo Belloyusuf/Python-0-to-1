8512e2979d8986d2ff218569c61a06e7fb7ac79a
bello yusuf <belloyusuf1997@gmail.com>
2023-11-14 16:18:42 +0100

    Done with lesson 1 on date and time.

--- BASIC TUTORIAL/10. Date and Time/Lesson1.py
+++ BASIC TUTORIAL/10. Date and Time/Lesson1.py
@@ -3,5 +3,10 @@ from datetime import date  # Import date from datetime
 
 # print the current date
 now = date.today()
-print(now)
+print("Current year and Date is: ", now)
 
+
+# Print the date of birth 
+birth_date = date(1997, 1, 20)  # Date of birth
+get_result = now - birth_date
+print("I spent " , get_result.days)
