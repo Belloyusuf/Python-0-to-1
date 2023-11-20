9c492c9028b9bf4772c5b19895ee992ade825ccf
bello yusuf <belloyusuf1997@gmail.com>
2023-11-20 15:25:38 +0100

    Working on lesson3 of the datetime.

---
+++ 10. Date and Time/Lesson3.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+
+#  Return a date corresponding to a date_string given in any valid 
+#  ISO 8601 format, except ordinal dates (e.g. YYYY-DDD):
+
+valid_date1 = date.fromisoformat('2020-12-04')
+print("Valid date 1", valid_date1)
+
+valid_date2 = date.fromisoformat('20201204')
+print("Valid date 2", valid_date2)
+
+valid_date3 = date.fromisoformat('2020-W01-04')
+print("valid date 3", valid_date3)
