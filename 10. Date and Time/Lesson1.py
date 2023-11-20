09d04eba7fc977bd88c1aa6977d3dd7b59c39235
bello yusuf <belloyusuf1997@gmail.com>
2023-11-20 15:49:43 +0100

    update lesson 1 on datetime

--- 10. Date and Time/Lesson1.py
+++ 10. Date and Time/Lesson1.py
@@ -1,4 +1,5 @@
 from datetime import date  # Import date from datetime
+import time  
 
 
 # print the current date
@@ -10,3 +11,8 @@ print("Current year and Date is: ", now)
 birth_date = date(1997, 1, 20)  # Date of birth
 get_result = now - birth_date
 print("I spent " , get_result.days)
+
+
+# Print current time
+localtime = time.localtime(time.time())
+print("Local current time is: ", localtime)
