e9505b6c1c9406b217916bea61eb66eefd01103e
bello yusuf <belloyusuf1997@gmail.com>
2023-11-20 15:39:45 +0100

    Lesson 4 on datetime were added. Its for calendar.

---
+++ 10. Date and Time/Lesson4.py
@@ -0,0 +1,11 @@
+import calendar
+
+
+"""
+The calendar module gives a wide range of methods to play with yearly and monthly
+calendars. Here, we print a calendar for a given month ( Jan 2020 ).
+"""
+cal = calendar.month(2020, 1)
+print ("Here is the calendar:")
+print (cal)
+
