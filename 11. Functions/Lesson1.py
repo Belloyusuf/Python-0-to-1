889cd4903dc58dcebdadbb8193193c306f4254a5
bello yusuf <belloyusuf1997@gmail.com>
2023-11-20 16:21:43 +0100

    Working on functions

--- 11. Functions/Lesson1.py
+++ 11. Functions/Lesson1.py
@@ -1,1 +1,7 @@
 #  FUNCTION 
+"""
+A function is a block of organized, reusable code that is used to perform a single, related
+action. Functions provide better modularity for your application and a high degree of code
+reusing.
+
+"""
