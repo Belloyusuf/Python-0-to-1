1064d76725e20ff5fc178957a18c0bfd7dde1bdd
bello yusuf <belloyusuf1997@gmail.com>
2023-11-22 06:17:01 +0100

    Done with  functions.

--- 11. Functions/Lesson1.py
+++ 11. Functions/Lesson1.py
@@ -8,6 +8,20 @@ reusing.
 
 # Built-in functions
 welcome = "Hello world"
-print(len(welcome) )# built in fuction
-print(max(welcome))
-print(min(welcome))
+print(len(welcome), "-", type(len))# built in fuction
+print(max(welcome), "-", type(max))
+print(min(welcome), "-", type(min))
+
+
+# Type conversion functions
+print(int(32))
+print(int("hello world"))
+
+""" float converts integers and strings to floating-point numbers: """
+print(float(32))
+print(float('3.14159'))
+""" Finally, str converts its argument to a string: """
+print(str(32))
+print(str(3.14159))
+
+
