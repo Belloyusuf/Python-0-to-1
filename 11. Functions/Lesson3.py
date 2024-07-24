3f7b7915977b8e8c8f48c30192278a35d2ac3614
bello yusuf <belloyusuf1997@gmail.com>
2024-07-24 21:33:56 +0100

    Done with example 3 on functions

--- 11. Functions/Lesson3.py
+++ 11. Functions/Lesson3.py
@@ -1,1 +1,9 @@
 # Lesson 3 on functions 
+def printString(str):
+    """ print the passed string """
+    print("I am the passed string", str)
+
+    return printString
+
+printString(" `Hello there!` ")
+
